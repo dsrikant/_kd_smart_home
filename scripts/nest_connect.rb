384692bfc0d63b9a4922037c9e76fa30d80b5e0c
Dushyant Srikant <dushyant.srikant@gmail.com>
2016-09-01 09:26:32 -0500

    Cleaned up nest_connect

--- scripts/nest_connect.rb
+++ scripts/nest_connect.rb
@@ -3,9 +3,22 @@ require 'rest-client'
 access_token = File.read("../config/access_token.secret").strip()
 access_pin = File.read("../config/access_pin.secret").strip()
 device_id = File.read("../config/device_id.secret").strip()
+structure_id = File.read("../config/structure_id.secret").strip()
 
-NEST_API_URL_GET_TEMP = "https://developer-api.nest.com/devices/thermostats/#{device_id}/target_temperature_f?auth=#{access_token}"
+NEST_API_URL_TEMP = "https://developer-api.nest.com/devices/thermostats/#{device_id}/target_temperature_f?auth=#{access_token}"
+NEST_API_URL_STATUS = "https://developer-api.nest.com/structures/#{structure_id}/away?auth=#{access_token}"
 
-response = RestClient.get(NEST_API_URL_GET_TEMP)
+target_temperature_f = '75'
+desired_status = 'away' # Can be home or away
 
-puts response
+def get_current_temperature
+  response = RestClient.get(NEST_API_URL_TEMP)
+end
+
+def set_temperature
+  response = RestClient.put(NEST_API_URL_TEMP, target_temperature_f)
+end
+
+def set_status
+  response = RestClient.put(NEST_API_URL_STATUS, desired_status)
+end
